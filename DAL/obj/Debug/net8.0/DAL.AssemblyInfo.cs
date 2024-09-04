ba323041d9e33ce32edcd37d58a2509431d36389
SaraGrinzaig <saragrinzaig@gmail.com>
2024-09-04 20:25:36 +0300

    add StatusRepository

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6667e564465596ed8eea5449e28e9a90626a7007")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8857f67131dc6d20b374544f011ee455724dd21")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
